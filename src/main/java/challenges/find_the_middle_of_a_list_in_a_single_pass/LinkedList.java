4764bb58bcf24fac270778a30fe3251392dcfa62
sousajo <josesousa@rentalcars.com>
2020-10-19 23:50:07 +0100

    Implemented palindrome challenge. Closes #1

---
+++ src/main/java/challenges/find_the_middle_of_a_list_in_a_single_pass/LinkedList.java
@@ -0,0 +1,67 @@
+package challenges.find_the_middle_of_a_list_in_a_single_pass;
+
+
+
+/**
+ * Created by sousaJ on 19/10/2020
+ * in package - challenges.find_the_middle_of_a_list_in_a_single_pass
+ **/
+public class LinkedList {
+
+    Node head;
+
+    public static void main(String[] args) {
+       LinkedList llist = new LinkedList();
+        for (int i = 0; i > 5; --i) {
+            llist.push(i);
+            llist.printNext();
+            llist.printMiddle(llist.head);
+        }
+//       llist.head = new Node(1);
+//       Node second = new Node(2);
+//       Node third = new Node(3);
+//       llist.head.next = second;
+//       second.next = third;
+//       llist.printNext();
+//       llist.printMiddle(llist.head);
+    }
+
+    public void push(int data){
+         Node node = new Node(data);
+         node.next = head;
+         head = node;
+    }
+
+    public void printNext(){
+         Node n = head;
+         while (n != null){
+             System.out.println(n.data + " ");
+             n = n.next;
+         }
+    }
+
+    public void printMiddle(Node head){
+         Node slow_ptr  = head;
+         Node fast_ptr = head;
+
+         if(head != null) {
+             while (fast_ptr != null && slow_ptr.next != null) {
+                    fast_ptr = fast_ptr.next.next;
+                    slow_ptr = slow_ptr.next;
+             }
+             System.out.println("The middle element is: " + slow_ptr.data);
+         }
+    }
+
+     static class Node {
+        int data;
+        Node next;
+
+         public Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+
+}
---
+++ src/test/java/challenges/find_the_middle_of_a_list_in_a_single_pass/MiddleNumberTest.java
@@ -0,0 +1,24 @@
+package challenges.find_the_middle_of_a_list_in_a_single_pass;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MiddleNumberTest {
+//    ListNode node1;
+
+    @BeforeEach
+    void setUp() {
+//       node1 = new ListNode(1);
+    }
+
+//            1 ==> 1
+//            1->2 ==> 1
+//            1->2->3->4 ==> 2
+//            1->2->3->4->5 ==> 3
+
+    @Test
+    void findMiddleNode() {
+
+//        assertEquals(1,  LinkedList.findMiddleNode(null));
+    }
+}
